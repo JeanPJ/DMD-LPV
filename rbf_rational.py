b9be50a97378fb2d78b50aacd928ff7c0dff38b4
JeanPJ <jeanpanaioti@gmail.com>
2025-04-24 18:18:02 -0300

    rbf update

--- rbf_rational.py
+++ rbf_rational.py
@@ -14,6 +14,25 @@ def k(p):
     return p[0]*p[1]/(p[0] + p[1])**2
 
 
+def rbf_basis(p,Nrbf):
+    
+    rbf_vec = np.empty([Nrbf,1])
+    
+    basis1 = np.array([[0,1]])
+    
+    basis2 = np.array([[1,0]])
+    
+    
+    for i in range(Nrbf):
+        
+        a1 = i/Nrbf
+        a2 = i/Nrbf
+        
+        c = a1*basis1 + a2*basis2
+        
+        rbf_vec[i] = np.exp(-gamma*np.linalg.norm(p - c))
+    return rbf_vec
+
 
 def rbf(p,w):
     
@@ -22,12 +41,22 @@ def rbf(p,w):
     Nrbf = w.shape[1]
     
     
-    rbf_vec = np.empty([Nrbf,1])
-    
-    
-    for i in range(Nrbf):
+    rbf_vec = rpf_basis(p)
+
+    return w@rbf_vec        
         
-        rbf_vec[i] = np.exp()        
+        
+
+
+N_data = 100    
+P = np.random.rand(2,N_data)
+
+
+Nrbf = 10
+
+
+    
+    
     
     
     

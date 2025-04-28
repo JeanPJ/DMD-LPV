0743701ce08a5801d6a1bb8a9e701b723da33ef4
JeanPJ <jeanpanaioti@gmail.com>
2025-04-28 10:46:22 -0300

    testing for new expetiment k(p)

--- rbf_rational.py
+++ rbf_rational.py
@@ -16,11 +16,13 @@ def k(p):
 
 def rbf_basis(p,Nrbf):
     
-    rbf_vec = np.empty([Nrbf,1])
+    gamma = 0.01
     
-    basis1 = np.array([[0,1]])
+    rbf_vec = np.empty([Nrbf,p.shape[1]])
     
-    basis2 = np.array([[1,0]])
+    basis1 = np.array([[0,1]]).T
+    
+    basis2 = np.array([[1,0]]).T
     
     
     for i in range(Nrbf):
@@ -30,18 +32,17 @@ def rbf_basis(p,Nrbf):
         
         c = a1*basis1 + a2*basis2
         
-        rbf_vec[i] = np.exp(-gamma*np.linalg.norm(p - c))
+        rbf_vec[i] = np.exp(-gamma*np.linalg.norm(p - c)**2)
     return rbf_vec
 
 
 def rbf(p,w):
     
-    gamma = 1
     
     Nrbf = w.shape[1]
     
     
-    rbf_vec = rpf_basis(p)
+    rbf_vec = rbf_basis(p,Nrbf)
 
     return w@rbf_vec        
         
@@ -52,8 +53,24 @@ N_data = 100
 P = np.random.rand(2,N_data)
 
 
-Nrbf = 10
+Nrbf = 50
 
+rbf_out = rbf_basis(P,Nrbf)
+
+rational_out = np.atleast_2d(k(P))
+
+
+U,s,VT = np.linalg.svd(rbf_out,full_matrices = False)
+
+V = VT.T
+
+reg = 0.01
+
+s_inv = np.diag(s/(s**2 + reg**2))
+
+theta = rational_out@V@s_inv@U.T 
+
+print(np.mean(np.abs((rational_out - theta@rbf_out)/rational_out)))
 
     
     

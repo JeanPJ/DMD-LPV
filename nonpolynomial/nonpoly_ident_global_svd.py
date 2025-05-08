51b350d97238b0eb210261883953a744790ec730
JeanPJ <jeanpanaioti@gmail.com>
2025-05-08 10:58:05 -0300

    didn't commit those last time

---
+++ nonpolynomial/nonpoly_ident_global_svd.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 15:00:46 2024
+
+@author: jeanpj
+"""
+
+from nonpolydata_plot import *
+from lpvs_ident import *
+
+import scipy.io as io
+import numpy as np
+
+w_elm = io.loadmat("elm_weights.mat")['W']
+
+
+
+
+def elm_basis(p,Nelm,W):
+    
+    
+    elm_vec = np.empty([Nelm,p.shape[1]])
+    
+    
+    bias = np.ones([1,p.shape[1]])
+    
+    p_and_bias = np.vstack([bias,p])
+    
+    
+    for i in range(Nelm):
+        
+        elm_vec[i] = np.tanh(W[i]@p_and_bias)
+    return elm_vec
+
+
+n_in = 1
+n_p = 2
+
+Nelm = 15
+
+k_fun = lambda p:elm_basis(p,Nelm,w_elm)
+
+diffusion_black_box_model = BlackBoxLPVS(n_in,n_p,number_of_states,k_fun,k_fun)
+
+
+#T_plot_lessdata = T_plot[:T_plot.shape[0]//2]
+
+
+T0 = 0*np.ones([number_of_states,1])
+T_plot_init = np.vstack([T0.T,T_plot[:-1,:]])
+
+
+
+parameter_data = p_signal
+
+print(parameter_data.shape)
+
+
+
+U_svd,s,V = diffusion_black_box_model.get_svd_from_data(T_plot_init.T,u_signal.T,parameter_data.T)
+    
+import scipy.io as io 
+
+
+svd_dict = {'U':U_svd,'s':s,'v':V}
+
+
+io.savemat('svd_from_input.mat',svd_dict)
+    
+    
+
+    
+    
